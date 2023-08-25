2fe3cf5f0d5b6f08ce9b185ac0d0ff1196bbf674
viralweybee <140693399+viralweybee@users.noreply.github.com>
2023-08-25 18:41:15 +0530

    Add files via upload

---
+++ todo/edit_delete.js
@@ -0,0 +1,6 @@
+let arr=['viral','aayush','dev','dabhi'];
+console.log(arr)
+arr[2]='jay';
+console.log(arr)
+arr.splice(2,1);
+console.log(arr);
--- todo/index.js
+++ todo/index.js
@@ -5,12 +5,12 @@ let addEl = document.getElementById('add');
 let searchEl = document.getElementById('search')
 let todo = document.getElementById('todo');
 let insertEl = document.getElementById('display_main');
-let selectEl=document.getElementById('select');
-let sortEl=document.getElementById('sort');
+let selectEl = document.getElementById('select');
+let sortEl = document.getElementById('sort');
 
 
 let arr = [];
-//-------------------------------------------------------------------------------------------Add data------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------Adddata-----------------------------------------------
 addEl.addEventListener('click', add);
 let count = 0;
 function add() {
@@ -22,10 +22,10 @@ function add() {
 document.addEventListener("keydown", function (e) {
     let key = e.key;
     if (key === "Enter") {
-       add();
+        add();
     }
 });
-// -----------------------------------------------------------------------------------------update uI-------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------updateuI------------------------------------------------
 function updateUI(arr) {
     console.log(arr);
     insertEl.innerHTML = "";
@@ -34,6 +34,7 @@ function updateUI(arr) {
         <div class="checked1">
    <input id="check${i + 1}"class="check" type="checkbox">
    <span id="text${i + 1}" class="text">${arr[i]}</span>
+   <input hidden id="text1${i+1}" class="text1">
    </div>
    <div class="checked2">
    <img class="edit" id="e${i + 1}" src="icons8-edit.svg" alt="">
@@ -42,94 +43,107 @@ function updateUI(arr) {
 </div>`
         insertEl.insertAdjacentHTML("afterbegin", html);
     }
-    edit(arr);
-    deleteval(arr);
+    edit();
+    deleteval();
 }
-//--------------------------------------------------------------------------------------edit functionality------------------------------------------------------------------------
-function edit(arr) {
+//------------------------------------------------------------------------------------editdeletefunctionality--------------------------------------
+function edit() {
     for (let i = 0; i < arr.length; i++) {
         if (document.getElementById(`e${i + 1}`)) {
             document.getElementById(`e${i + 1}`).addEventListener('click', () => {
-                if(todo.value!=""){
-                document.getElementById(`text${i + 1}`).innerHTML = todo.value;
-                arr[i] = todo.value;
-                todo.value="";
+                if (todo.value != "") {
+                    document.getElementById(`text${i + 1}`).innerHTML = todo.value;
+                    arr[i] = todo.value;
+                    todo.value = "";
+                    i = -1;
                 }
             })
         }
     }
 }
-function deleteval(arr){
-    for(let i=0;i<arr.length;i++){
-    if (document.getElementById(`d${i+1}`)) {
-        document.getElementById(`d${i+1}`).addEventListener('click', () => {
-            if (document.getElementById(`div${i+1}`)) {
-                arr.splice(i,1);
-                document.getElementById(`div${i+1}`).remove();
-            }
-        })
+function deleteval() {
+    for (let i = 0; i < arr.length; i++) {
+        if (document.getElementById(`d${i + 1}`)) {
+            document.getElementById(`d${i + 1}`).addEventListener('click', () => {
+                if (document.getElementById(`div${i + 1}`)) {
+                    let text = "Press a button!\nEither OK or Cancel.";
+                    if (confirm(text) == true) {
+                        arr.splice(i, 1);
+                        console.log(arr);
+                        document.getElementById(`div${i + 1}`).remove();
+                        i = -1;
+                    }  
+                }
+            })
+        }
     }
 }
-}
 //------------------------------------------------------------------------------------------action-------------------------------------------------------------------------------
-selectEl.addEventListener('click',select);
-function select(value){
-    let temp=[...arr];
+selectEl.addEventListener('click', select);
+function select(value) {
+    let temp = [...arr];
     if (value === 'select') {
         for (let i = 0; i < temp.length; i++) {
             document.getElementsByClassName('check')[i].checked = true;
         }
     }
-    else if(value==='unselect'){
-        for(let i=0;i<temp.length;i++){
-            document.getElementsByClassName('check')[i].checked=false;
+    else if (value === 'unselect') {
+        for (let i = 0; i < temp.length; i++) {
+            document.getElementsByClassName('check')[i].checked = false;
         }
     }
-    else if(value==='delete'){
-       var length= document.getElementsByClassName('check').length;
-        for(let i=0;i<length;i++){
-            if (document.getElementsByClassName('check')[i].checked) {
-                if(document.getElementsByClassName('checked')[i]){
-                document.getElementsByClassName('checked')[i].remove();
-                arr.splice(i-1,1);
+    else if (value === 'delete') {
+        var length = arr.length;
+        let temp = [];
+        for (let i = 1; i <= length; i++) {
+            if (document.getElementById(`check${i}`)) {
+                if (document.getElementById(`check${i}`).checked) {
+                    document.getElementById(`div${i}`).remove();
+                    temp.push(arr[i - 1]);
                 }
-                length = document.getElementsByClassName("check").length;
-                i--;
+            }
+        }
+        for (let i = 0; i < temp.length; i++) {
+            for (let j = 0; j < arr.length; j++) {
+                if (temp[i] === arr[j]) {
+                    arr.splice(j, 1);
+                    break;
+                }
             }
         }
     }
     selectEl.value = 'Action'
 }
 // ---------------------------------------------------------------------------------------------------sort------------------------------------------------------------------------
-sortEl.addEventListener('click',sort);
-function sort(value){
-    
-    if(value==='A-Z'){
-        let temp=[...arr];
+sortEl.addEventListener('click', sort);
+function sort(value, arr) {
+
+    if (value === 'A-Z') {
+        let temp = [...arr];
         temp.sort();
         temp.reverse();
         updateUI(temp);
     }
-    else if(value==='Z-A'){
-        let temp=[...arr];
+    else if (value === 'Z-A') {
+        let temp = [...arr];
         temp.sort();
         updateUI(temp);
     }
-    else if(value==='oldest'){
-        let temp=[...arr];
+    else if (value === 'oldest') {
+        let temp = [...arr];
         temp.reverse();
         updateUI(temp);
     }
-    else if(value==='newest'){
-        let temp=[...arr];
+    else if (value === 'newest') {
+        let temp = [...arr];
         updateUI(temp);
     }
 }
-//------------------------------------------------------------------------------------------------all active completed------------------------------------------------------------
-allbtnEl.addEventListener('click',()=>{
-    for(let i=0;i<arr.length;i++){
-        if (document.getElementById(`check${i+1}`)) {
-            document.getElementById(`div${i+1}`).style.display = "flex";
+//-------------------------------------------------------------------------------------allactivecompleted------------------------------------------
+allbtnEl.addEventListener('click', () => {
+    for (let i = 0; i < arr.length; i++) {
+        if (document.getElementById(`check${i + 1}`)) {
+            document.getElementById(`div${i + 1}`).style.display = "flex";
         }
     }
 })
@@ -157,35 +171,35 @@ completebtnEl.addEventListener("click", () => {
         }
     }
 });
-// ---------------------------------------------------------------------------------------search---------------------------------------------------------------------------------
-searchEl.addEventListener('click',search);
-function search(){
-    let map=new Map();
-    let text=todo.value;
-    for(let i=1;i<=arr.length;i++){
-        if(document.getElementById(`text${i}`).textContent.includes(text)){
-            map.set(i,text);
+// ---------------------------------------------------------------------------------------search----------------------------------------------------
+searchEl.addEventListener('click', search);
+function search() {
+    let map = new Map();
+    let text = todo.value;
+    for (let i = 1; i <= arr.length; i++) {
+        if (document.getElementById(`text${i}`).textContent.includes(text)) {
+            map.set(i, text);
         }
     }
     display(map);
 }
-function display(map){
-    for(let i=1;i<=arr.length;i++){
-        if(!map.has(i)){
-            if(document.getElementById(`div${i}`)){
-                document.getElementById(`div${i}`).style.display="none";
+function display(map) {
+    for (let i = 1; i <= arr.length; i++) {
+        if (!map.has(i)) {
+            if (document.getElementById(`div${i}`)) {
+                document.getElementById(`div${i}`).style.display = "none";
             }
         }
     }
 }
-document.addEventListener("keydown",function(e){
-    let key=e.key;
-    if(key==="Backspace"&&document.getElementById('todo').value.length==1){
-        for(let i=1;i<=arr.length;i++){
-            if(document.getElementById(`div${i}`)){
-                document.getElementById(`div${i}`).style.display="flex";
+document.addEventListener("keydown", function (e) {
+    let key = e.key;
+    if (key === "Backspace" && document.getElementById('todo').value.length == 1) {
+        for (let i = 1; i <= arr.length; i++) {
+            if (document.getElementById(`div${i}`)) {
+                document.getElementById(`div${i}`).style.display = "flex";
             }
         }
     }
 })
-// -------------------------------------------------------------------------------------code complete---------------------------------------------------------------------------
+// ----------------------------------------------------------------------codecomplete---------------------------------------------------------------
